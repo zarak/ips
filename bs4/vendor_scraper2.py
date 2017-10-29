682fdbbdbc5db99bedb5396bfd4cce2afe537dfc
Zarak Mahmud <zarak.mahmud@utoronto.ca>
2017-10-29 18:25:59 +0500

    Store current search term in var and add categories

--- bs4/vendor_scraper2.py
+++ bs4/vendor_scraper2.py
@@ -40,6 +40,9 @@ class VendorFinderScraper(object):
                 'State': 'ctl00_ContentPlaceHolder1_ddlState',
                 'Construction Codes': 'ctl00_ContentPlaceHolder1_lboxSicCodes',
                 'Construction License': 'ctl00_ContentPlaceHolder1_ddlConstructionLicense'
+                'Construction/Design Services': 'ctl00_ContentPlaceHolder1_lboxConstructionServices',
+                'Limitations': 'ctl00_ContentPlaceHolder1_ddlLimitations',
+                'Work Classification/License Specialty': 'ctl00_ContentPlaceHolder1_lboxLicense'
                 }
         return id_dict[category]
 
@@ -51,7 +54,6 @@ class VendorFinderScraper(object):
         col_length = len(df.columns)
         if col_length > 7:
             df = df.drop(range(8, col_length), axis=1)
-        # df = df.drop([8, 9], axis=1)
         df = df.drop(df.index[:3])
         df = df.drop(df.index[-2:])
         print(df.head())
@@ -76,12 +78,16 @@ class VendorFinderScraper(object):
         self.driver.get(self.url)
     
         # Select state selection dropdown
+        print("Initializing search...")
         select = Select(self.driver.find_element_by_id(self.search_id))
         option_indexes = range(1, len(select.options))
         # option_indexes = range(1, 4)
 
         # Iterate through each state
         for index in option_indexes:
+            current_search_term = select.options[index].text
+            print("Searching for vendors by {}={}".format(self.category,
+                current_search_term))
             select.select_by_index(index)
             self.driver.find_element_by_id('ctl00_ContentPlaceHolder1_btnSearch').click()
 
@@ -93,6 +99,7 @@ class VendorFinderScraper(object):
             pageno = 2
 
             while True:
+                print("Scraping page number", pageno)
                 s = BeautifulSoup(self.driver.page_source, "lxml")
 
                 df = self.parse_page(s)
@@ -114,10 +121,18 @@ class VendorFinderScraper(object):
                     '//a[@href="javascript:__doPostBack(\'ctl00$ContentPlaceHolder1$gvVendorList\',\'Page${}\')"]'.format(pageno-1))))
 
                 pageno += 1
+            # Add column of current search term here
+            df[self.category] = current_search_term
+            print("#"*20)
+            print("Dataframe for", current_search_term)
+            print(df.head())
+            print("#"*20)
 
             # Reset search for next state
             select = self.next_state()
         columns = [item.text for item in s.findAll('th')]
+        columns.append(self.category)
+        print("Constructing final dataframe...")
         final_df = self.final_dataframe(dataframes, columns)
         final_df = self.contact_info_columns(final_df)
         final_df.to_csv('ins_scraped.csv', index=False)
@@ -126,5 +141,5 @@ class VendorFinderScraper(object):
 
 
 if __name__ == '__main__':
-    scraper = VendorFinderScraper('Construction Codes')
+    scraper = VendorFinderScraper()
     scraper.scrape()
